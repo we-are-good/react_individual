0a8574f0c01e3435ed6dc38b1ac34580e22e6bf0
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-01-23 11:49:57 +0900

    todolist upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,164 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toDoList, setToDoList] = useState([
+    {index : 1, context : "let", project : "Javascript", isDone : false,},
+    {index : 2, context : "let", project : "JSX", isDone : false,},
+    {index : 3, context : "let", project : "UX", isDone : false}
+  ]);
+  const [alreadyDoneList, setalreadyDoneList] =useState([])
+
+  const [context, setContext] = useState('')
+  const [project, setProject] = useState('')
+
+  const contextHandle = (event) => {
+    setContext(event.target.value);
+  }
+  const projectHandle = (event) => {
+    setProject(event.target.value);
+  }
+
+  const newListUp = () => {
+    const newList={
+    index: toDoList.length+1,
+    context: context,
+    project: project,
+    isDone: false,}
+    setToDoList([...toDoList, newList])
+  }
+
+  const removeList = (item) => {
+    const lessList = toDoList.filter((toDoList) => toDoList.index !== item)
+    const alreadyLessList = alreadyDoneList.filter((toDoList) => toDoList.index !== item)
+    setToDoList(lessList);
+    setalreadyDoneList(alreadyLessList);
+  }
+
+  const completedList = (item) => {
+    const doneListToToDoList = toDoList.filter((toDoList) => toDoList.index === item);
+    doneListToToDoList[0].isDone = true;
+    const shiftTodoList = toDoList.filter((toDoList) => toDoList.index !== item);
+
+    setalreadyDoneList([...alreadyDoneList, ...doneListToToDoList]);
+    setToDoList(shiftTodoList)
+  }
+
+  const shiftCompletedList = (item) => {
+    const reAdditionList = alreadyDoneList.filter((toDoList) => toDoList.index === item)
+    reAdditionList[0].isDone = false;
+    const shiftDoneList = alreadyDoneList.filter((toDoList) => toDoList.index !== item)
+    
+    setToDoList([...toDoList, ...reAdditionList])
+    setalreadyDoneList(shiftDoneList)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='top-app-style'>
+      <div className='header-app-style'>
+        <header>
+          <div className='header-name'>
+          <div> My Todo List</div>
+          <div> React</div>
+          </div>
+        </header> 
+        
+            <div className='section-name'>
+              Addition
+            </div>
+          <div className='app-style'>
+
+              <div className='total-style'>
+                <div className='contents-style'>
+                  <div className='input-data-style'>
+                    <input type="text" placeholder='제목'
+                      value = {context}
+                      onChange={contextHandle}/>
+                  </div>
+                  <div className='input-data-style'>
+                      <input type="text" placeholder='내용'
+                        value = {project}
+                        onChange={projectHandle}/>
+                  </div>
+                  <div className="input-data-style">
+                    <button onClick= {() => {newListUp()
+                      setContext("")
+                      setProject("")}}>
+                      추가하기</button>
+                  </div>
+                </div >
+              </div>
+          </div>
+
+            <div className='section-name'>
+              working
+            </div>
+
+      {toDoList.map((list) => {
+      return (
+        <div className='app-style'>
+          <div className='total-style'>
+          <div className='contents-style'>
+          <div className='input-data-style'>
+            {list.context}
+          </div>
+          
+          <div className='input-data-style'>
+          { list.project } : { list.isDone }
+          </div>
+
+          <div className='input-data-style'>
+          <button key = {list.index} 
+            onClick = {() => {
+            removeList(list.index)}}>
+            삭제하기
+          </button>
+          </div>
+
+          <div className='input-data-style'>
+          <button onClick = {() => {
+          completedList(list.index)
+          }}> 완료 </button>
+          </div>
+          </div>
+          </div>
+          </div>
+      )})}
+      
+      <div className='section-name'>
+        Done
+      </div>
+
+      {alreadyDoneList.map((list) => {return (
+      <div className='app-style'>
+        <div className='total-style'>
+        <div className='contents-style'>
+
+        <div className='input-data-style'>
+        {list.context}
+        </div>
+        <div className='input-data-style'>
+        {list.project} : {list.isDone}
+        </div>
+        <div className='input-data-style'>
+        <button key = {list.index} 
+            onClick = {() => {
+              removeList(list.index)}}>
+            삭제하기
+        </button>
+        </div>
+        <div className='input-data-style'>
+        <button onClick = {() => {
+          shiftCompletedList(list.index)
+        }}> 취소 </button>
+        </div>
+
+        </div>
+        </div>
+
+        </div>
+        )})}
+       </div>
     </div>
   );
 }
